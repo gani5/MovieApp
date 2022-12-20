3a5d185dca6cab5e886016250d2a9f3f06400175
Ganesh <ganesh.reactdeveloper@gmail.com>
2022-12-20 20:26:44 +0530

    checkpoint2

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div>
+      <div className='headerContainer'>
+        <img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" alt="Ex" className="mediaImg" />
+      </div>
+    </div>
+  )
+}
